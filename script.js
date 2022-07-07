bd4a88637a77c961a643ecedc7d906dacdcb5567
ID <izaakduncan@outlook.com>
2022-07-07 03:46:55 -0700

    functional build

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ data to change:
 - background colors for bottom and top sections (text will automatically format)
 */
 
-// figure out icon, fontawesome
+// Element selection
+const generateButton = document.getElementById('generateButton')
+const headlineText = document.getElementById('headlineText')
+const headerSubText = document.getElementById('headerSubText')
+const bottomDiv = document.getElementById('bottomDiv')
+
+generateButton.addEventListener('click', function() {
+    bottomDiv.className = `w3-row-padding w3-${colors[Math.floor(Math.random() * colors.length)]} w3-padding-64 w3-container`
+    headlineText.innerHTML = `${headlineArray[Math.floor(Math.random() * headlineArray.length)]} TECH`
+    headerSubText.innerHTML = `WE ${verbs[Math.floor(Math.random() * verbs.length)]} THE MOST ${adjectives[Math.floor(Math.random() * adjectives.length)]} TECHNOLOGY`
+})
---
+++ wordBank.js
@@ -0,0 +1,7 @@
+let headlineArray = ['COOL','HAPTIC', 'ENCODED', 'PRIMARY', 'NETWORK', 'SUPER', 'EXCELLENT', 'MASTER']
+
+let adjectives = ['SKILLED', 'TECHNICAL', 'MODERN', 'PROCEDURAL', 'EMPIRICAL', 'DYNAMIC']
+
+let verbs = ['ACTIVATE', 'DEVELOP', 'ENGINEER', 'DESIGN', 'DEPLOY', 'BIRTH', 'ENVISION']
+
+let colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'amber', 'brown', 'deep-purple', 'blue-gray', 'indigo', 'light-blue', 'pale-blue', 'dark-gray', 'light-green', 'lime']
