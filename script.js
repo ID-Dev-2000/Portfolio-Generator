1ae0794684075b2b0703298e57e4e9a5478151eb
ID <izaakduncan@outlook.com>
2022-07-07 03:58:02 -0700

    cleanup

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-create arrays containing words, values
-randomly generate number from count of each array
-new variable to select object from array using number value
-insert new variable into template literal to insert array object into string
-variable in template literal will display random array object
-*/
-
-/*
-data to change:
-- main name
-- header text in body
-- subtext in body
-- icon color
-- background colors for bottom and top sections (text will automatically format)
-*/
-
 // Element selection
 const generateButton = document.getElementById('generateButton')
 const headlineText = document.getElementById('headlineText')
